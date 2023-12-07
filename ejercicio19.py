d8c43ae6a3253511f538543525f6c3a060280ee1
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-07 16:37:44 -0600

    Se le pregunta al usuario si desea ver los numeros del 1 al 5, de lo contrario el programa termina usando 'while'

---
+++ ejercicio19.py
@@ -0,0 +1,8 @@
+numero = 1
+resp = "si"
+while resp == "si":
+        print(numero)
+        print("-Si desea ver los numeros del 1 al 5 responda si: \n-Si desea terminar responda no: \n")
+        resp = input("")
+        print("\n")
+        numero+=1
