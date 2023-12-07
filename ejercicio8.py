ce5c3474c741f9ad56760d65636a5e1a3de73824
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-07 16:02:20 -0600

    Ejercicio que  muestra el residuo y el cociente de dos numeros enteros

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+n = int(input("Ingrese un numero entero: "))
+m = int(input("ingrese un segundo numero entero:"))
+
+c = n/m
+res = c * m
+r = n - res
+
+print("El cociente es: ", c)
+print("El resto es: ", r)
