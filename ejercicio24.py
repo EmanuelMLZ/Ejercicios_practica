ec8eae3b807114bb3635766abd2becb95790802d
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-07 16:53:54 -0600

    Ejercicio que muestra la suma y el producto  de todos los numeros del 1 al 20

---
+++ ejercicio24.py
@@ -0,0 +1,10 @@
+numeros = 10
+suma = 0
+producto1 = 1
+
+while numeros <= 20:
+    suma = suma + numeros
+    producto1 = producto1 * numeros
+    numeros = numeros + 2
+print("La suma es: ", suma)
+print("El producto es : ",producto1 )
