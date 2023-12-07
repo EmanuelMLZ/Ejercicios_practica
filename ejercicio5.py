6e427a04878ec8e06612c48e0244f78c2ed89f8c
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-07 15:45:28 -0600

    Ejercicio donde se muestra el calculo del sueldo de un empleado, tomando en cuenta la cantidad de horas trabajadas y su coste por hora

---
+++ ejercicio5.py
@@ -0,0 +1,4 @@
+sueldo = float(input("Ingrese la cantidad de horas trabajadas: "))
+c_hora = float(input("Ingrese el coste por hora: "))
+pago = sueldo * c_hora
+print("Tu sueldo diario es :$ ",pago)
