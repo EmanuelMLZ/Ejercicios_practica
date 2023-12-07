dad55cd6f97239cd8938f7523c49096586307025
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-07 16:08:16 -0600

    Ejercicio que calcula el capital total obtenido en una cantidad invertida, con el interes anual y la cantidad de años

---
+++ ejercicio9.py
@@ -0,0 +1,7 @@
+Can_i = float(input("Ingrese una cantidad a invertir :"))
+interes_a = float(input("Ingrese el interes anual % :"))
+can_años = int(input("Ingrese la cantidad de años: "))
+
+cap_ob = Can_i * interes_a * can_años
+
+print("El capital obtenido es:", cap_ob)
