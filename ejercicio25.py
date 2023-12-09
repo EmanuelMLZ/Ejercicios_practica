eb234e8046bd9d57f7c7a3cef1babf10d5b00175
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-08 22:03:53 -0600

    Mostrando todos los numeros negativos del 1 al 5

--- ejercicio25.py
+++ ejercicio25.py
@@ -1,7 +1,11 @@
 nPos = 0
+nNeg = 0
 for x in range (5):
     numero = int(input("Ingrese 5 numeros: "))
     print(numero)
     if numero > 0:
         nPos +=1
+    elif numero < 0:
+        nNeg +=1
 print("Los numeros positivos son: ", nPos)
+print("Los numeros negativos son: ", nNeg)
