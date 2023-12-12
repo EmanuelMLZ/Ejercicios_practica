b9c57fed8870fa19a9b3c18cbf614182c1623299
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-11 18:13:28 -0600

    Reparando errores del codigo jaja que insano que soy

--- ejercicio26.py
+++ ejercicio26.py
@@ -1,13 +1,16 @@
 total = 0
-articulo = print(input("¿Que articulo desea? :"))
+print("\n")
+articulo = (input("¿Que articulo desea? :"))
 resp = "si"
+print("\n")
 
 while resp == "si":
     Num_art = int(input("Ingrese el numero de articulos que desea: "))
     Precio_art = float(input("Ingrese el precio del articulo: "))
     sub_total = Precio_art  *  Num_art
     total = total + sub_total
+    print("\n")
     print("- Si desea continuar responda si: \n- Si desea terminar responda no: \n")
-    print(resp)
+    resp = input("")
     print("\n")
     print("Su total a pagar es: ", total)
