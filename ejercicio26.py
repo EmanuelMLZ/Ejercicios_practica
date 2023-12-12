1c58c257f3a56603154ba6c342985f125a1e800d
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-12 12:45:40 -0600

    Arreglando errores de sintaxis y redondeando decimales

--- ejercicio26.py
+++ ejercicio26.py
@@ -1,16 +1,17 @@
 total = 0
-print("\n")
-articulo = (input("¿Que articulo desea? :"))
 resp = "si"
-print("\n")
 
 while resp == "si":
+    print("\n")
+    articulo = (input("¿Que articulo desea? : "))
     Num_art = int(input("Ingrese el numero de articulos que desea: "))
     Precio_art = float(input("Ingrese el precio del articulo: "))
     sub_total = Precio_art  *  Num_art
     total = total + sub_total
+    total = round(total,2)
+    print("\n")
+    print("Su total a pagar es: ", total)
     print("\n")
     print("- Si desea continuar responda si: \n- Si desea terminar responda no: \n")
     resp = input("")
     print("\n")
-    print("Su total a pagar es: ", total)
