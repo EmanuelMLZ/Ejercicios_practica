1142d4dc94c4b59024bbb187440df22ca5232f63
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-13 19:15:55 -0600

    Mejorando sintaxis y aumentando el ciclo de 3 trabajadores a 5

--- ejercicio27.py
+++ ejercicio27.py
@@ -3,7 +3,7 @@ Nsueldo = 0
 nomina_aum = 0
 nomina_sueldo = 0
 I = 1
-for x in range (3):
+for x in range (5):
     print("")
     nombre = str(input("Ingrese su nombre: "))
     codigo = str(input("Ingrese su codigo: "))
@@ -18,12 +18,16 @@ for x in range (3):
         aumento = sueldo * 0.09
         Nsueldo = sueldo + aumento
     print("")
-    print(nombre, "", codigo, "", sueldo, "", Nsueldo)
+    print("Nombre: " ,nombre, "", "Codigo: " ,codigo, "", "Sueldo actual: ", sueldo)
+    print("")
+    print("Su nuevo sueldo es: ",Nsueldo)
+    print("")
+    print("Su aumento de sueldo es de: ",aumento)
     print("")
     nomina_aum = nomina_aum + aumento
     nomina_sueldo = nomina_sueldo + Nsueldo
     I+=1
-    print("El total de aumentos es: ", nomina_aum)
-    print("")
-    print("El total de nomina es: ", nomina_sueldo)
-    print("")
+print("El total de aumentos es: ", nomina_aum)
+print("")
+print("El total de nomina es: ", nomina_sueldo)
+print("")
