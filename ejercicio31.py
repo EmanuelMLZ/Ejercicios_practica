04b23955d8dffedb25fe2ca0b25981837c8c836b
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-21 12:02:36 -0600

    Almacenando asignaturas en una lista y luego imprimiendolas una despues de la otra

--- ejercicio31.py
+++ ejercicio31.py
@@ -1,2 +1,9 @@
-asignaturas = ['Lenguaje Quimica Matematica Fisica' ]
-print("Yo estudio: ",asignaturas)
+mensaje  = ""
+asignaturas = ["Lenguaje", "Quimica", "Fisica", "Historia"]
+print("Yo estudio: ",asignaturas[0])
+mensaje = input("")
+print("Yo estudio: ",asignaturas[1])
+mensaje = input("")
+print("Yo estudio: ",asignaturas[2])
+mensaje = input("")
+print("Yo estudio: ",asignaturas[3])
