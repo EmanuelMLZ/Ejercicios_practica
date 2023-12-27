c9473862dc3ecc48980d68f7e81dd84760935b46
Emanuel Martinez <emanuelalejandrom4@gmail.com>
2023-12-27 17:07:38 -0600

    Almacenando asignaturas en una lista y su nota correspondiente tambien, luego imprimiendo la materia y su nota correspondiente

---
+++ ejercicio33.py
@@ -0,0 +1,15 @@
+materias = ["Matematica", "Fisica", "Quimica", "Historia", "Lenguaje"]
+print("")
+Nota_mat = float(input("Ingrese la nota de Matematica: "))
+Nota_Fis = float(input("Ingrese la nota de FIsica: "))
+Nota_Qui = float(input("Ingrese la nota de Quimica: "))
+Nota_His = float(input("Ingrese la nota de Historia: "))
+Nota_Leng = float(input("Ingrese la nota de Lenguaje: "))
+print("")
+Notas = [Nota_mat, Nota_Fis, Nota_Qui, Nota_His, Nota_Leng]
+print("Materia: ", materias[0], (""),("")  ,   "Nota: ", Notas[0])
+print("Materia: ", materias[1], (""),("")  ,   "Nota: ", Notas[1])
+print("Materia: ", materias[2], (""),("")  ,   "Nota: ", Notas[2])
+print("Materia: ", materias[3], (""),("")  ,   "Nota: ", Notas[3])
+print("Materia: ", materias[4], (""),("")  ,   "Nota: ", Notas[4])
+print("")
